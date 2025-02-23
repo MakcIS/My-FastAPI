2e9275486878de2c5978b8ac91033a885984eb89
Maxim Yakushev <makcis@list.ru>
2025-02-24 06:09:48 +0700

    templates

--- CRUD/crud.py
+++ CRUD/crud.py
@@ -1,29 +1,66 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Body, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
-messages_db = {0: "First post in FastAPI"}
+class Message(BaseModel):
+    id: int | None = None
+    text: str
+    model_config = {
+        "json_schema_extra": {
+            "examples":
+            [
+                {
+                    "text": "Simple message"
+                }
+            ]
+        }
+    }
+
+messages_db = []
 
 @app.get('/')
-async def get_all_messages() -> dict:
-    return messages_db
+async def get_all_messages(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(request, 'message.html', {'messages': messages_db})
 
-@app.get('/message/{message_id}')
-async def get_message(message_id:int) -> str:
-    return messages_db[message_id]
+@app.get(path='/message/{message_id}')
+async def get_message(request: Request, message_id: int) -> HTMLResponse:
+    try:
+        return templates.TemplateResponse(request, 'message.html', {'message': messages_db[message_id]})
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Message not found')
 
-@app.post("/message")
-async def create_message(message:str) -> str:
-    pass
+@app.post("/message", status_code=status.HTTP_201_CREATED)
+async def create_message(message:Message) -> str:
+    if messages_db:
+        message.id = max(messages_db, key=lambda x: x.id).id + 1
+    else:
+        message.id = 0
+    messages_db.append(message)
+    return 'Message created!'
 
 @app.put("/message/{message_id}") 
-async def update_message(message_id:int, message:str) -> str:
-    pass
+async def update_message(message_id:int, message:str = Body()) -> str:
+    try:
+        edit_message = messages_db[message_id]
+        edit_message.text = message
+        return "Message updated!"
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Message not found')
 
 @app.delete("/message/{message_id}")
 async def delete_message(message_id:int) -> str:
-    pass
-
+    try:
+        messages_db.pop(message_id)
+        return f"Message ID={message_id} deleted!"
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Message not found')
+        
 @app.delete("/")
 async def kill_message_all()->str:
-    pass
+    messages_db.clear()
+    return "All messages deleted!"
